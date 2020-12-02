cc569556b70152d846ff406b58e3f8b807f39c50
louis <louissentinella@gmail.com>
2020-12-02 12:48:37 +0000

    dayTwo baby

--- dayOne/dayOne.py
+++ dayOne/dayOne.py
@@ -1,23 +1,29 @@
+import time
+
+start = time.time()
+
 file = open("numbers.txt", "r")
 numbersList = []
 for i in file:
     numbersList.append(i.strip('\n'))
 
+
 def partOne(numbersList):
     for i in numbersList:
         for j in numbersList:
-            if int(i) + int(j)  == 2020:
-                return(int(i)*int(j) )
+            if int(i) + int(j) == 2020:
+                return int(i) * int(j)
+
 
 def partTwo(numbersList):
     for i in numbersList:
         for j in numbersList:
             for k in numbersList:
-                if int(i) + int(j) +int(k) == 2020:
-                    return (int(i)*int(j) * int(k))
-
-
+                if int(i) + int(j) + int(k) == 2020:
+                    return int(i) * int(j) * int(k)
 
 
 print(partOne(numbersList))
 print(partTwo(numbersList))
+
+print(time.time() - start)
---
+++ dayTwo/dayTwo.py
@@ -0,0 +1,40 @@
+file = open("passwords.txt", "r")
+passwordsList = []
+for i in file:
+    i = i.strip('\n')
+    i = i.split()
+    i[0] = i[0].split('-')
+    i[1] = i[1].strip(':')
+    passwordsList.append(i)
+
+
+def partOne(passwordsList):
+    correctCounter = 0
+    for i in passwordsList:
+        counter = 0
+        for letter in i[2]:
+            if letter == i[1]:
+                counter += 1
+        if int(i[0][0]) <= counter <= int(i[0][1]):
+            correctCounter += 1
+    return correctCounter
+
+
+def partTwo(passwordsList):
+    correctCounter = 0
+    for i in passwordsList:
+        if i[2][int(i[0][0]) - 1] == i[1]:
+            if i[2][int(i[0][1]) - 1] == i[1]:
+                pass
+            else:
+                correctCounter += 1
+        elif i[2][int(i[0][1]) - 1] == i[1]:
+            if i[2][int(i[0][0]) - 1] == i[1]:
+                pass
+            else:
+                correctCounter += 1
+    return correctCounter
+
+
+print(partOne(passwordsList))
+print(partTwo(passwordsList))
