3ece028e4cd214f95ba404a386f4f78b6aeb5f32
louis <louissentinella@gmail.com>
2020-12-02 15:14:23 +0000

    optimised with some code from atto

--- dayTwo/dayTwo.py
+++ dayTwo/dayTwo.py
@@ -3,20 +3,17 @@ import time
 start = time.time()
 
 file = open("passwords.txt", "r")
-passwordsFileList = []
+passwordsList = []
 for i in file:
     i = i.strip('\n').split()
     i[0], i[1] = i[0].split('-'), i[1].strip(':')
-    passwordsFileList.append(i)
+    passwordsList.append(i)
 
 
 def partOne(passwordsList):
     correctCounter = 0
     for i in passwordsList:
-        counter = 0
-        for letter in i[2]:
-            if letter == i[1]:
-                counter += 1
+        counter = len([letter for letter in i[2] if letter == i[1]])
         if int(i[0][0]) <= counter <= int(i[0][1]):
             correctCounter += 1
     return correctCounter
@@ -24,15 +21,15 @@ def partOne(passwordsList):
 
 def partTwo(passwordsList):
     correctCounter = 0
+
     for i in passwordsList:
-        if (i[2][int(i[0][0]) - 1] == i[1]) != (i[2][int(i[0][1]) - 1] != i[1]):
+        if (i[2][int(i[0][0]) - 1] == i[1]) != (i[2][int(i[0][1]) - 1] == i[1]):
             correctCounter += 1
+
     return correctCounter
 
 
-print(partOne(passwordsFileList))
-print(partTwo(passwordsFileList))
+print(partOne(passwordsList))
+print(partTwo(passwordsList))
 
 print(str(round(time.time() - start, 5)) + " seconds")
-
-file.close()
