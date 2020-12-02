092f84a753088ff271ade0ff7a6820f1d84a7df9
louis <louissentinella@gmail.com>
2020-12-02 01:03:12 +0000

    ahabd

--- dayOne/dayOne.py
+++ dayOne/dayOne.py
@@ -2,7 +2,6 @@ file = open("numbers.txt", "r")
 numbersList = []
 for i in file:
     numbersList.append(i.strip('\n'))
-#print(numbersList)
 
 def partOne(numbersList):
     for i in numbersList:
@@ -14,7 +13,6 @@ def partTwo(numbersList):
     for i in numbersList:
         for j in numbersList:
             for k in numbersList:
-                #print(i +j)
                 if int(i) + int(j) +int(k) == 2020:
                     return (int(i)*int(j) * int(k))
 
